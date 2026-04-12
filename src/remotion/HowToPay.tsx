2f2d6e05e8713ec66bcc5d4f30beafdbdcc15f4f
elias <eliashabibhamidabdu.gmail.com>
2026-04-12 15:22:16 +0300

    style: move pasted number to the top and increase size/spacing

--- src/remotion/HowToPay.tsx
+++ src/remotion/HowToPay.tsx
@@ -103,8 +103,8 @@ export const HowToPay: React.FC = () => {
                         style={{ top: '56.5%', height: 70 }}
                     />
                     
-                    <div className="absolute top-[43%] left-[58%] -translate-x-1/2 -translate-y-1/2 w-[60%] h-16 flex items-center px-4">
-                        <span className="text-gray-900 font-black text-5xl tracking-widest">
+                    <div className="absolute top-[30%] left-[58%] -translate-x-1/2 -translate-y-1/2 w-[60%] h-20 flex items-center px-4">
+                        <span className="text-gray-900 font-black text-6xl tracking-[0.2em]">
                             {frame > 150 ? '0963138123' : ''}
                         </span>
                     </div>
